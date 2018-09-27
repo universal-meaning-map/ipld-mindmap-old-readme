034d24c6850db8225d5d37da438b8433ebd891c0
Xavi <xavings@gmail.com>
2018-09-27 11:31:28 +0000

    Converted project into CRA

---
+++ src/IpfsController.js
@@ -0,0 +1,35 @@
+//const createNode = require ('./createNode.js')
+const IPFS = require('ipfs')
+let ipfs
+const data = {
+    name: 'Unga'
+}
+
+function createNode(callback) {
+    ipfs = new IPFS()
+    ipfs.on('start', () => {
+        onStart(ipfs)
+    })
+}
+
+function onStart(ipfs) {
+    ipfs.dag.put(data, { format: 'dag-cbor', hashAlg: 'sha2-256' }, onPut)
+}
+
+function onPut(err, cid) {
+    if (err) throw err
+
+    ipfs.dag.get(cid, (err, dag) => {
+        if (err) throw err
+        console.log(JSON.stringify(dag.value))
+    })
+}
+
+function onGet(err, result) {
+    if (err) throw err
+
+    console.log(JSON.stringify(result.value))
+}
+
+
+createNode()
--- src/index.js
+++ src/index.js
@@ -1,36 +1,8 @@
-//const createNode = require ('./createNode.js')
-const IPFS = require('ipfs')
-let ipfs
-const data = {
-    name: 'Unga'
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-function createNode(callback) {
-    ipfs = new IPFS()
-    ipfs.on('start', () => {
-        onStart(ipfs)
-    })
-
-}
-
-function onStart(ipfs) {
-    ipfs.dag.put(data, { format: 'dag-cbor', hashAlg: 'sha2-256' }, onPut)
-}
-
-function onPut(err, cid) {
-    if (err) throw err
-
-    ipfs.dag.get(cid, (err, dag) => {
-        if (err) throw err
-        console.log(JSON.stringify(dag.value))
-    })
-}
-
-function onGet(err, dag) {
-    if (err) throw err
-
-    console.log(JSON.stringify(result.value))
-}
-
-
-createNode()
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
