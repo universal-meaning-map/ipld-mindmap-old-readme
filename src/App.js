a121d6798427ad96335524e7440d51721c472e5d
Xavi <xavings@gmail.com>
2018-09-27 17:42:06 +0000

    Importing previous app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 import './IpfsController'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import React, { Component } from 'react'
+import  IPLDRender  from 'ipld-mindmap-ptsjs-render'
 
-export default App;
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pauseAnimation: false,
+            currentZoom: 1,
+            lastZoom: 1
+        }
+    }
+
+    handleChange(event) {
+        this.setState({ variance: event.target.value });
+    }
+
+    handleClick(event) {
+        //this.setState({ pauseAnimation: !this.state.pauseAnimation });
+    }
+
+    onPinchStart(e) {
+        this.setState({ lastZoom: this.state.currentZoom })
+    }
+    onPinchMove(e) {
+        let zoomDelta = e.zoom
+        let currentZoom = this.state.lastZoom * zoomDelta
+        this.setState({ currentZoom: currentZoom })
+    }
+
+    render() {
+        return (
+
+            <div
+                className="App"
+                style={{ touchAction: 'none' }}
+                onClick={this.handleClick.bind(this)}>
+
+                <div><IPLDRender
+                    name="IPLDRender"
+                    background="#fff"
+                    onPinchStart={this.onPinchStart.bind(this)}
+                    onPinchMove={this.onPinchMove.bind(this)}
+                    zoom={this.state.currentZoom}
+                    loop={true} />
+                </div>
+
+            </div>
+        );
+    }
+}
