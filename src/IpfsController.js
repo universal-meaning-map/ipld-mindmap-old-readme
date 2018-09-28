77c09b5eee057c8c98497455229d72195e6c14c8
Xavi <xavings@gmail.com>
2018-09-28 15:35:33 +0000

    Testing stuff

--- src/IpfsController.js
+++ src/IpfsController.js
@@ -5,15 +5,26 @@ const data = {
     name: 'Unga'
 }
 
+
+//const hash = 'QmcDXEwVJoJ7G1bjhCFzo5Z6gtADS1qCHp8EA33NYqgJyK'
+const hash = 'zdpuAvYJaZxBjTV4WH3irwThm5t2a7yTccoN9cWpDmtV4CiNz'
+
 function createNode(callback) {
     ipfs = new IPFS()
+   
     ipfs.on('start', () => {
+        console.log (ipfs)
         onStart(ipfs)
     })
 }
 
 function onStart(ipfs) {
-    ipfs.dag.put(data, { format: 'dag-cbor', hashAlg: 'sha2-256' }, onPut)
+    //ipfs.dag.put(data, { format: 'dag-cbor', hashAlg: 'sha2-256' }, onPut)
+    console.log('started')
+    ipfs.dag.get(hash, (err, dag) => {
+        if (err) throw err
+        console.log(JSON.stringify(dag.value))
+    })
 }
 
 function onPut(err, cid) {
